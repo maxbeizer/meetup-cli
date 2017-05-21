440b9de2ba0b8ed2feecf8498bcc772382a3288f
max beizer <max.beizer@gmail.com>
2017-05-21 17:35:32 -0500

    Add cleaner implemenation of no auth

--- __mocks__/auth.js
+++
@@ -1,3 +0,0 @@
-const password = undefined
-
-module.exports = {password}
--- test/no-auth.test.js
+++ test/no-auth.test.js
@@ -1,8 +1,9 @@
-/* global test, expect, jest */
+/* global test, expect */
 
-jest.mock('../auth')
+const auth = require('../auth')
+const {NO_PASSWORD_ERROR_MSG} = auth
+Object.defineProperty(auth, 'password', { value: null })
 const {fetchResults} = require('../client')
-const {NO_PASSWORD_ERROR_MSG} = require('../auth')
 
 test('throws an auth error if no password in netrc', () => {
   expect(() => fetchResults()).toThrow(NO_PASSWORD_ERROR_MSG)
